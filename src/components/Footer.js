8d67696654eb5ab5da7cf9589fa9d4076164fd0e
Jake Ascher <ascherj@gmail.com>
2020-12-10 16:38:40 -0500

    Update social media buttons in Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -56,32 +56,13 @@ export default class Footer extends React.Component {
                 </section>
               </div>
               <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
-                  <img
-                    src={facebook}
-                    alt="Facebook"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="twitter" href="https://twitter.com">
-                  <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="instagram" href="https://instagram.com">
+                <a
+                  title="instagram"
+                  href="https://www.instagram.com/thehumanitybook/"
+                >
                   <img
                     src={instagram}
                     alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
